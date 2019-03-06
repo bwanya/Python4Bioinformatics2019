08d6b213ddf7eed12d9cfe843ffa3cae2ef33e0d
User <user@user>
2019-03-06 16:52:13 +0300

    Exercise_3

---
+++ Notebooks/Python_exercises/Exercise_2.py
@@ -0,0 +1,6 @@
+#!/home/user/miniconda3/bin/python
+
+## Exercise 2 The square of prime numbers between 1&100
+
+Prime_numbers=range(1,100)
+[i**2 for i in range(1,100) if i%3==0]
---
+++ Notebooks/Python_exercises/Exercise_3.py
@@ -0,0 +1,5 @@
+#!/home/user/miniconda3/bin/python
+
+## Using strings, lists, tuples and dictionaries concepts, find the reverse complement of AAAAATCCCGAGGCGGCTATATAGGGCTCCGGAGGCGTAATATAAAA
+
+
